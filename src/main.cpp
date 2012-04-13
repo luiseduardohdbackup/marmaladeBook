cb4d7efb974954b5304f89f14b2a0d6f1c219d41
Putilov Michael <post.snowy@gmail.com>
2012-04-13 13:40:09 +0600

    subprojects added

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,16 @@
-/*
- * This file is part of the Marmalade SDK Code Samples.
- *
- * Copyright (C) 2001-2011 Ideaworks3D Ltd.
- * All Rights Reserved.
- *
- * This source code is intended only as a supplement to Ideaworks Labs
- * Development Tools and/or on-line documentation.
- *
- * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
- * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
- * PARTICULAR PURPOSE.
- */
 #include "s3e.h"
+#include "IwGx.h"
+#include "Iw2D.h"
 
 // "main" is the S3E entry point
-int main()
-{
+int main() {
 	// to exit correctly, applications should poll for quit requests
-	while(!s3eDeviceCheckQuitRequest())
-	{
-	
+	while(!s3eDeviceCheckQuitRequest())	{
+		
+		s3eDebugOutputString("Hello!");
 		// S3E applications should yield frequently
 		s3eDeviceYield();
 	}
-
 
 	return 0;
 }
